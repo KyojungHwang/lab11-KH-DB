2a414292b064b0f5a4c698d5fdde3a8eb381418a
Dennis Bamaca Perez <dennis.bamacaper@ufl.edu>
2026-08-03 05:53:30 -0400

    partner 2

--- calculator.py
+++ calculator.py
@@ -9,21 +9,21 @@ def add(a, b):
     return a + b
 
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
 
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
     return b / a
 
 
-def logarithm(a, b):
+def log(a, b):
     if a <= 0 or a == 1:
         raise ValueError("Invalid logarithm base.")
     if b <= 0:
@@ -31,5 +31,5 @@ def logarithm(a, b):
     return math.log(b, a)
 
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
