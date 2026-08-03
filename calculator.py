3c1c21db77eb1e1ab85fa78c7e3e4f5877cef6d0
Dennis Bamaca Perez <dennis.bamacaper@ufl.edu>
2026-08-03 11:52:01 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -27,12 +27,6 @@ def mul(a, b):
     return a * b
 
 
-def div(a, b):
-    if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero.")
-    return b / a
-
-
 def log(a, b):
     if a <= 0 or a == 1:
         raise ValueError("Invalid logarithm base.")
